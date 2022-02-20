fe6d58cb9f5e5428d8e9e156bd547a33347a61b0
Your Name <you@example.com>
2022-02-20 17:20:36 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <strong>Falling Cone Woodworking</strong>
+       <p>Under construction</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
